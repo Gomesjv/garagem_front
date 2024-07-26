fbfa31847bed69a95866453300f5aa68ab826ad8
Gomesjv <j.vitor10gomes@gmail.com>
2024-07-26 16:08:36 -0300

    Adicionando passage

---
+++ src/components/VeiculoCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps(["item"])
+</script>
+
+<template>
+    <div class="card">
+        <img v-if="item.foto_veiculo" :src="item.foto_veiculo.url" alt="" width="300px">
+
+        <div class="info">
+            <p>
+                {{ item.modelo.nome }} - {{ item.ano }}
+            </p>
+
+            <p>
+                R$ {{ item.preco }}
+            </p>
+
+            <p>
+                {{ item.cor.descricao }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .card {
+        width: 300px;
+        padding: 10px 0;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+        border-radius: 10px;
+    }
+
+    .info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+    }
+</style>    
---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import "@passageidentity/passage-elements/passage-auth";
+</script>
+
+<template>
+  <div class="authContainer">
+    <passage-auth app-id="jzaFaCWeZSTVVpdeANt8LKsG"></passage-auth>
+  </div>
+</template>
+
+<style></style>
